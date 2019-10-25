ce94120c4e74a41aaad0bf74e927364a26d3488f
Tim Heuer <tim@timheuer.com>
2019-10-24 21:23:30 -0700

    formatting mail more

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -3,21 +3,26 @@
 exports.__esModule = true;
 var github = require("@actions/github");
 var sgMail = require('@sendgrid/mail');
+var moment = require('moment');
+var md = require('remarkable');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 var context = github.context;
 var issue = context.payload.issue;
+var posted_date = moment(issue.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
 var msgBody = "";
 msgBody = 'NOTICE: BREAKING CHANGE FOR ' + issue.repository;
 msgBody = msgBody + '\nANNOUNCEMENT URL: ' + issue.url;
+msgBody = msgBody + '\nPOSTED AT: ' + posted_date;
 msgBody = msgBody + '\n\n\nTITLE: ' + issue.title;
-msgBody = msgBody + '\nDetails:';
-msgBody = msgBody + '\n' + issue.body;
-msgBody = msgBody + '\n\n\n' + issue.created_at;
+msgBody = msgBody + '\nDetails:\n\n';
+var issueBodyPlain = issue.body;
+var issueBodyHtml = md.render(msgBody) + md.render(issue.body);
 var msg = {
     to: 'timheuer@microsoft.com',
     from: '.NET Breaking Changes <timheuer@microsoft.com>',
-    subject: 'GitHub Issue Notification for Issue' + issue.number,
-    text: msgBody
+    subject: 'BC Notification: ' + issue.number,
+    text: msgBody + issueBodyPlain,
+    html: issueBodyHtml
 };
 sgMail
     .send(msg)
--- sendgrid.ts
+++ sendgrid.ts
@@ -2,23 +2,29 @@
 import github = require('@actions/github');
 
 const sgMail = require('@sendgrid/mail');
+const moment = require('moment');
+const md = require('remarkable');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const context = (github as any).context
 const issue = context.payload.issue
 
+var posted_date = moment(issue.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
+
 var msgBody = "";
 msgBody = 'NOTICE: BREAKING CHANGE FOR ' + issue.repository;
 msgBody = msgBody + '\nANNOUNCEMENT URL: ' + issue.url;
+msgBody = msgBody + '\nPOSTED AT: ' + posted_date;
 msgBody = msgBody + '\n\n\nTITLE: ' + issue.title;
-msgBody = msgBody + '\nDetails:';
-msgBody = msgBody + '\n' + issue.body
-msgBody = msgBody + '\n\n\n' + issue.created_at;
+msgBody = msgBody + '\nDetails:\n\n';
+var issueBodyPlain = issue.body;
+var issueBodyHtml = md.render(msgBody) + md.render(issue.body); 
 
 const msg = {
     to: 'timheuer@microsoft.com',
     from: '.NET Breaking Changes <timheuer@microsoft.com>',
     subject: 'GitHub Issue Notification for Issue' + issue.number,
-    text: msgBody
+    text: msgBody + issueBodyPlain,
+    html: issueBodyHtml
 };
 
 sgMail
