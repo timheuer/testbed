4c697c19e19b69fa60185643e042b23a18f8e712
Tim Heuer <tim@timheuer.com>
2019-10-24 22:00:27 -0700

    label

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -11,21 +11,34 @@ var context = github.context;
 var issue = context.payload.issue;
 console.log(issue);
 var posted_date = moment(issue.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
-var msgBody = "";
-msgBody = 'NOTICE: BREAKING CHANGE FOR ' + issue;
-msgBody = msgBody + '\nANNOUNCEMENT URL: ' + issue.html_url;
-msgBody = msgBody + '\nPOSTED AT: ' + posted_date;
-msgBody = msgBody + '\n\n\nTITLE: ' + issue.title;
-msgBody = msgBody + '\nDetails:\n\n';
-var issueBodyPlain = issue.body;
-var issueBodyHtml = 'Posted at: ' + posted_date + '<br/>Annnouncement URL: <a href=' + issue.html_url + '>' + issue.html_url + '</a><br/><br/>' + md.render(issue.body);
-var msg = {
-    to: 'timheuer@microsoft.com',
-    from: '.NET Breaking Changes <timheuer@microsoft.com>',
-    subject: 'GitHub Issue Notification for Issue' + issue.number,
-    text: issueBodyPlain,
-    html: issueBodyHtml
-};
-sgMail
-    .send(msg)
-    .then(function () { return console.log('Mail sent successfully'); })["catch"](function (error) { return console.error(error.toString()); });
+var isBC = false;
+// check to see if it is a bc
+var labels = issue.labels;
+labels.array.forEach(function (label) {
+    if (label.name == 'breaking-change') {
+        isBC = true;
+    }
+});
+if (isBC) {
+    var msgBody = "";
+    msgBody = 'NOTICE: BREAKING CHANGE FOR ' + issue;
+    msgBody = msgBody + '\nANNOUNCEMENT URL: ' + issue.html_url;
+    msgBody = msgBody + '\nPOSTED AT: ' + posted_date;
+    msgBody = msgBody + '\n\n\nTITLE: ' + issue.title;
+    msgBody = msgBody + '\nDetails:\n\n';
+    var issueBodyPlain = issue.body;
+    var issueBodyHtml = 'Posted at: ' + posted_date + '<br/>Annnouncement URL: <a href=' + issue.html_url + '>' + issue.html_url + '</a><br/><br/>' + md.render(issue.body);
+    var msg = {
+        to: 'timheuer@microsoft.com',
+        from: '.NET Breaking Changes <dotnetbcn@microsoft.com>',
+        subject: 'GitHub Issue Notification for Issue' + issue.number,
+        text: issueBodyPlain,
+        html: issueBodyHtml
+    };
+    sgMail
+        .send(msg)
+        .then(function () { return console.log('Mail sent successfully'); })["catch"](function (error) { return console.error(error.toString()); });
+}
+else {
+    console.log('Not a breaking change');
+}
--- sendgrid.ts
+++ sendgrid.ts
@@ -10,25 +10,42 @@ const context = (github as any).context
 const issue = context.payload.issue
 console.log(issue);
 var posted_date = moment(issue.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
+var isBC = false;
 
-var msgBody = "";
-msgBody = 'NOTICE: BREAKING CHANGE FOR ' + issue;
-msgBody = msgBody + '\nANNOUNCEMENT URL: ' + issue.html_url;
-msgBody = msgBody + '\nPOSTED AT: ' + posted_date;
-msgBody = msgBody + '\n\n\nTITLE: ' + issue.title;
-msgBody = msgBody + '\nDetails:\n\n';
-var issueBodyPlain = issue.body;
-var issueBodyHtml = 'Posted at: ' + posted_date + '<br/>Annnouncement URL: <a href=' + issue.html_url + '>' + issue.html_url + '</a><br/><br/>' + md.render(issue.body); 
+// check to see if it is a bc
+var labels = issue.labels;
+labels.array.forEach(label => {
+    if (label.name == 'breaking-change')
+    {
+        isBC = true;
+    }
+});
 
-const msg = {
-    to: 'timheuer@microsoft.com',
-    from: '.NET Breaking Changes <timheuer@microsoft.com>',
-    subject: 'GitHub Issue Notification for Issue' + issue.number,
-    text: issueBodyPlain,
-    html: issueBodyHtml
-};
+if (isBC)
+{
+    var msgBody = "";
+    msgBody = 'NOTICE: BREAKING CHANGE FOR ' + issue;
+    msgBody = msgBody + '\nANNOUNCEMENT URL: ' + issue.html_url;
+    msgBody = msgBody + '\nPOSTED AT: ' + posted_date;
+    msgBody = msgBody + '\n\n\nTITLE: ' + issue.title;
+    msgBody = msgBody + '\nDetails:\n\n';
+    var issueBodyPlain = issue.body;
+    var issueBodyHtml = 'Posted at: ' + posted_date + '<br/>Annnouncement URL: <a href=' + issue.html_url + '>' + issue.html_url + '</a><br/><br/>' + md.render(issue.body); 
 
-sgMail
-    .send(msg)
-    .then(() => console.log('Mail sent successfully'))
-    .catch(error => console.error(error.toString()));
+    const msg = {
+        to: 'timheuer@microsoft.com',
+        from: '.NET Breaking Changes <dotnetbcn@microsoft.com>',
+        subject: 'GitHub Issue Notification for Issue' + issue.number,
+        text: issueBodyPlain,
+        html: issueBodyHtml
+    };
+
+    sgMail
+        .send(msg)
+        .then(() => console.log('Mail sent successfully'))
+        .catch(error => console.error(error.toString()));
+}
+else
+{
+    console.log('Not a breaking change');
+}
